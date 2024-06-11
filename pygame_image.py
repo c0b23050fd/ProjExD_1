82f8fef2234edb9d425cc8ad0db22f0c65f085e2
yoshiki <c0b23050fd@edu.teu.ac.jp>
2024-06-11 16:49:43 +0900

    koukaton move myself

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,8 @@ def main():
     bg_img_f = pg.transform.flip(bg_img_f, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (300, 200)
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -22,9 +24,18 @@ def main():
         screen.blit(bg_img, [-(tmr%3200), 0])
         screen.blit(bg_img_f, [-(tmr%3200) + 1600, 0])
         screen.blit(bg_img, [-(tmr%3200) + 3200, 0])
-        kk_rct = kk_img.get_rect()
-        kk_rct.center = (300, 200)
         screen.blit(kk_img, kk_rct)
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1,0)
+
         pg.display.update()
         tmr += 1      
         clock.tick(200)
