d4a47cb2ae4af040ef85e52ea0d38a2dc851b786
yoshiki <c0b23050fd@edu.teu.ac.jp>
2024-06-11 16:05:47 +0900

    1~5まで

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        kk_rct = kk_img.get_rect()
+        kk_rct.center = (300, 200)
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
